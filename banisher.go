576065495a7a107d55b4a71020c15d47f31eefe5
Stéphane Depierrepont <toorop@toorop.fr>
2019-04-17 15:51:43 +0200

    add persistence

---
+++ banisher.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/dgraph-io/badger"
+
+	"github.com/coreos/go-iptables/iptables"
+)
+
+// Banisher is THE banisher
+type Banisher struct {
+	sync.Mutex
+	db  *badger.DB
+	IPT *iptables.IPTables
+}
+
+func NewBanisher() (b *Banisher, err error) {
+	b = new(Banisher)
+
+	// badger
+	options := badger.DefaultOptions
+	options.Dir = fmt.Sprintf("%s/db.bdg", home)
+	options.ValueDir = fmt.Sprintf("%s/db.bdg", home)
+	options.SyncWrites = true
+	options.Logger = nil
+
+	b.db, err = badger.Open(options)
+	if err != nil {
+		return nil, err
+	}
+
+	// iptables binding
+	b.IPT, err = iptables.NewWithProtocol(iptables.ProtocolIPv4)
+	if err != nil {
+		return nil, err
+	}
+	return
+}
+
+// Add ban an IP
+func (b *Banisher) Add(ip, ruleName string) {
+	var err error
+
+	// IP validation
+	ok := net.ParseIP(ip)
+	if ok == nil {
+		return
+	}
+
+	b.Lock()
+	defer b.Unlock()
+
+	// Already in DB ?
+	found := false
+	err = b.db.View(func(txn *badger.Txn) error {
+		_, err := txn.Get([]byte(ip))
+		if err != nil {
+			if err == badger.ErrKeyNotFound {
+				err = nil
+			}
+			return err
+		}
+		found = true
+		return nil
+	})
+	if err != nil {
+		log.Println("failed to check in DB:", err)
+		return
+	}
+	if found {
+		return
+	}
+
+	// iptables
+	if err = b.IPT.AppendUnique("filter", "INPUT", "-s", ip, "-j", "DROP"); err != nil {
+		log.Println("failed to ad iptable rule:", err)
+		return
+	}
+
+	// add to badger
+	err = b.db.Update(func(txn *badger.Txn) error {
+		return txn.Set([]byte(ip), []byte(strconv.FormatInt(time.Now().Add(time.Duration(180)*time.Minute).Unix(), 10)))
+	})
+
+	if err != nil {
+		log.Printf("failed to add %s in db: %s", ip, err)
+		// remove from iptables
+		if err = b.IPT.Delete("filter", "INPUT", "-s", ip, "-j", "DROP"); err != nil {
+			log.Printf("failed to remove %s from iptables: %s", ip, err)
+		}
+		return
+	}
+	log.Printf("%s: %s banned", ruleName, ip)
+}
+
+// Remove unban an IP
+func (b *Banisher) Remove(ip string) {
+	var err error
+	if err = b.IPT.Delete("filter", "INPUT", "-s", ip, "-j", "DROP"); err != nil {
+		log.Printf("failed to delete iptables rules for %s : %s", ip, err)
+		return
+	}
+	err = b.db.Update(func(txn *badger.Txn) error {
+		return txn.Delete([]byte(ip))
+	})
+	if err != nil {
+		log.Printf("failed to remove %s from db: %s", ip, err)
+		return
+	}
+	log.Printf("%s unbanned", ip)
+}
+
+// Restore restore iptables rules from DB
+func (b Banisher) Restore() error {
+	err := b.db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchSize = 20
+		it := txn.NewIterator(opts)
+		defer it.Close()
+		for it.Rewind(); it.Valid(); it.Next() {
+			ip := it.Item().Key()
+			if err := b.IPT.AppendUnique("filter", "INPUT", "-s", string(ip), "-j", "DROP"); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	return err
+}
+
+// GC remove expired bannishment
+func (b *Banisher) GC() {
+	for {
+		now := time.Now().Unix()
+		err := b.db.View(func(txn *badger.Txn) error {
+			opts := badger.DefaultIteratorOptions
+			opts.PrefetchSize = 20
+			it := txn.NewIterator(opts)
+			defer it.Close()
+			for it.Rewind(); it.Valid(); it.Next() {
+				ip := it.Item().Key()
+				err := it.Item().Value(func(val []byte) error {
+					ts, err := strconv.ParseInt(string(val), 10, 64)
+					if err != nil {
+						return err
+					}
+					if ts < now {
+						go b.Remove(string(ip))
+					}
+					return nil
+				})
+				if err != nil {
+					log.Println("failed to parse string:", err)
+				}
+			}
+			return nil
+		})
+		if err != nil {
+			log.Println("failed in GC loop:", err)
+		}
+		time.Sleep(time.Duration(5) * time.Minute)
+	}
+}
--- main.go
+++ main.go
@@ -1,160 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"net"
-	"regexp"
-	"strings"
-	"sync"
+	"os"
+	"path/filepath"
 	"time"
 
-	"github.com/coreos/go-iptables/iptables"
 	"github.com/coreos/go-systemd/sdjournal"
 )
 
 var banisher *Banisher
-
-var reTmail *regexp.Regexp
-
-// Banisher is THE banisher
-type Banisher struct {
-	sync.Mutex
-	banished map[string]int64
-	IPT      *iptables.IPTables
-}
-
-func NewBanisher() (b *Banisher, err error) {
-	b = new(Banisher)
-	b.IPT, err = iptables.NewWithProtocol(iptables.ProtocolIPv4)
-	if err != nil {
-		return nil, err
-	}
-	b.banished = make(map[string]int64)
-	return
-}
-
-func (b *Banisher) Add(ip string) {
-	var err error
-	// validation
-	ok := net.ParseIP(ip)
-	if ok == nil {
-		return
-	}
-	b.Lock()
-	defer b.Unlock()
-
-	// iptables
-	if err = b.IPT.AppendUnique("filter", "INPUT", "-s", ip, "-j", "DROP"); err != nil {
-		log.Println("add iptable rule failed:", err)
-		return
-	}
-	b.banished[ip] = time.Now().Add(time.Duration(180) * time.Minute).Unix()
-	log.Printf("%s added", ip)
-}
-
-func (b *Banisher) Remove(ip string) error {
-	log.Printf("%s removed", ip)
-	if err := b.IPT.Delete("filter", "INPUT", "-s", ip, "-j", "DROP"); err != nil {
-		return err
-	}
-	delete(b.banished, ip)
-	return nil
-}
-
-func (b *Banisher) WatchBannishedTime() {
-	for {
-		now := time.Now().Unix()
-		for ip, ts := range b.banished {
-			if ts < now {
-				if err := b.Remove(ip); err != nil {
-					log.Printf("b.Remove(%s) failed: %s", ip, err)
-				}
-			}
-		}
-		time.Sleep(time.Duration(1) * time.Minute)
-	}
-}
-
-// parser is the ... parser
-type parser struct {
-	rules []rule
-}
-
-func (p parser) Write(in []byte) (n int, err error) {
-	n = len(in)
-	entry := string(in)
-	//log.Println(entry)
-	for _, rule := range p.rules {
-		if rule.Match.Match(in) {
-			ip := rule.RegexIP.FindString(entry)
-			log.Printf("%s match %s", ip, rule.Name)
-			go banisher.Add(ip)
-			break
-		}
-	}
-	return n, nil
-}
-
-// implements Write interface
-func (p parser) WriteOld(in []byte) (l int, err error) {
-	l = len(in)
-	entry := string(in)
-	log.Println(entry)
-	ip := ipv4Regex.FindString(entry)
-	log.Println(ip)
-	// dovecot
-	if strings.Contains(entry, "imap-login:") && strings.Contains(entry, "auth failed") {
-		parts := strings.Split(entry, ",")
-		if len(parts) < 4 {
-			return
-		}
-
-		jetteIP := strings.Split(strings.TrimSpace(parts[3]), "=")
-		if len(jetteIP) != 2 {
-			return
-		}
-		go banisher.Add(jetteIP[1])
-		//
-	} else if strings.Contains(entry, "smtpd") && (strings.Contains(entry, "ERROR auth") || strings.Contains(entry, "-client timeout")) {
-		// time="2019-04-16T09:56:10.220728971+02:00" level=info msg="smtpd d4e055ce0a2b07c2f6989f3b5578b98eb0a84a8f-141.98.80.30:17606-client timeout"
-
-		ip := reTmail.FindString(entry)
-		if ip != "" {
-			go banisher.Add(ip)
-		}
-
-	}
-	return l, nil
-}
+var home string
 
 // main
 func main() {
 	var err error
 
-	// get rules
-	rules, err := parseRules("./rule.yml")
+	// get home (working path)
+	home, err = filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln("failed to os.Getwd():", err)
 	}
 
-	log.Println(rules)
+	// get rules
+	rules, err := parseRules(fmt.Sprintf("%s/rule.yml", home))
+	if err != nil {
+		log.Fatalln("failed to parse rules:", err)
+	}
+
+	// init banisher
 	banisher, err = NewBanisher()
 	if err != nil {
 		log.Fatalln(err)
 	}
+	defer banisher.db.Close()
 
-	// unban IP
-	go banisher.WatchBannishedTime()
+	// restore rule from DB
+	if err = banisher.Restore(); err != nil {
+		log.Fatalln(err)
+	}
+
+	// remove expired iptables rules
+	go banisher.GC()
 
 	r, err := sdjournal.NewJournalReader(sdjournal.JournalReaderConfig{
 		Since: time.Duration(-5) * time.Second,
 	})
-
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln("failed to get journal reader:", err)
 	}
 	if r == nil {
-		log.Fatalln("reader is nil")
+		log.Fatalln("journal reader is nil")
 	}
 	defer r.Close()
 
